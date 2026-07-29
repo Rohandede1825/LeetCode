8efba18893c1ad2d5a13cb8dbca9830753d843cb
Rohan Gangadhar Dede <150899788+Rohandede1825@users.noreply.github.com>
2026-07-29 18:53:50 +0530

    Time: 237 ms (19.35%), Space: 47 MB (74.19%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -1,66 +1,107 @@
+
 class Solution {
+
+    private static final int LIMIT = 1_000_001;
+    private int[] primes;
+
     public String smallestPalindrome(String s, int k) {
+        buildPrimes(5000);
+
         int[] freq = new int[26];
-        int n = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        int[] half = new int[26];
+        char mid = 0;
+        int halfLen = 0;
+
+        for (int i = 0; i < 26; i++) {
+            half[i] = freq[i] / 2;
+            halfLen += half[i];
+            if ((freq[i] & 1) == 1) mid = (char) ('a' + i);
+        }
+
+        if (countWays(half) < k) return "";
+
+        StringBuilder left = new StringBuilder();
+
+        for (int pos = 0; pos < halfLen; pos++) {
+            for (int c = 0; c < 26; c++) {
+                if (half[c] == 0) continue;
+
+                half[c]--;
+                int ways = countWays(half);
+
+                if (ways >= k) {
+                    left.append((char) ('a' + c));
+                    break;
+                } else {
+                    k -= ways;
+                    half[c]++;
+                }
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        ans.append(left);
+        if (mid != 0) ans.append(mid);
+        ans.append(left.reverse());
+
+        return ans.toString();
+    }
+
+    private int countWays(int[] cnt) {
+        int total = 0;
+        for (int x : cnt) total += x;
+
+        long res = 1;
+
+        for (int p : primes) {
+            if (p > total) break;
+
+            int e = factExp(total, p);
+            for (int x : cnt) {
+                if (x > 1) e -= factExp(x, p);
+            }
+
+            while (e-- > 0) {
+                if (res > LIMIT / p) return LIMIT;
+                res *= p;
+            }
+        }
+
+        return (int) Math.min(res, LIMIT);
+    }
+
+    private int factExp(int n, int p) {
+        int e = 0;
+        while (n > 0) {
+            n /= p;
+            e += n;
+        }
+        return e;
+    }
+
+    private void buildPrimes(int n) {
+        boolean[] comp = new boolean[n + 1];
         int cnt = 0;
-        long totalWays = 1L;
-        for(int i=0;i<n/2;i++){
-            freq[s.charAt(i)-'a']++;
-        }
 
-        char[] alpha = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        };
-        StringBuilder sb = new StringBuilder();
-        outer : for(int i=25;i>=0;i--){
-            if(freq[i]==0)continue;
-            int j=1;
-            while(j<=freq[i]){
+        for (int i = 2; i <= n; i++) {
+            if (!comp[i]) {
                 cnt++;
-                totalWays = (totalWays*cnt)/j;
-                if(totalWays>k){
-                    for(int l = 0;l<i;l++){
-                        while(freq[l]>0){
-                            sb.append(alpha[l]);
-                            freq[l]--;
-                        }
+                if ((long) i * i <= n) {
+                    for (int j = i * i; j <= n; j += i) {
+                        comp[j] = true;
                     }
-                    while(freq[i]>j){
-                        sb.append(alpha[i]);
-                        freq[i]--;
-                    }
-                    break outer;
-                }
-                j++;
-            }
-        }
-        if(k>totalWays)return "";
-        
-        
-        for(int i=0;i<cnt;i++){
-            for(char ch= 'a';ch<='z';ch++){
-                int j = ch-'a';
-                if(freq[j]==0)continue;
-                if(k<=(totalWays*freq[j])/(cnt-i)){
-                   totalWays = (totalWays*freq[j])/(cnt-i);
-                   freq[j]--;
-                   sb.append(ch);
-                   break;
-                }
-                else{
-                    k = (int)(k - (totalWays*freq[j])/(cnt-i));
                 }
             }
         }
-        if(n%2==1){
-            sb.append(s.charAt(n/2));
-        }
 
-        for(int i=n/2-1;i>=0;i--){
-            sb.append(sb.charAt(i));
+        primes = new int[cnt];
+        int idx = 0;
+        for (int i = 2; i <= n; i++) {
+            if (!comp[i]) primes[idx++] = i;
         }
-
-        return sb.toString();
     }
 }
